20bdbb31f57a75568e464164eb6341143acfb298
Ken Sheedlo <ovrkenthousand@gmail.com>
2013-08-13 18:33:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# firebase-hackathon
+
+Some app that uses a realtime backend. I don't know yet.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('firebaseHackathonApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
