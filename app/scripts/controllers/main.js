23b2acd11a3567e1fcfef31c9ecb820da31c41c9
Ken Sheedlo <ovrkenthousand@gmail.com>
2013-08-13 20:24:25 -0700

    fill in index controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('firebaseHackathonApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+  .controller('MainCtrl', ['$scope', 'angularFire', 'angularFireAuth', 
+    function ($scope, angularFire, angularFireAuth) {
+      var API_BASE = 'https://api.github.com/';
+
+      $scope.issues = [];
+      $scope.get_issues = function() {
+        var profile = 'angular';
+        var project = 'angular.js';
+        var issues_url = API_BASE + profile + '/' + project + '/issues';
+
+        $http.get(issues_url).success(function(data) {
+          $scope.issues = data;
+        });
+      };
+    }]);
