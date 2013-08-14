424844d19a51bbddde16a5d93fde7ba174404ae1
Dean Sofer <proloser@hotmail.com>
2013-08-13 21:51:49 -0700

    Basic implementation of writing from firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,27 +1,41 @@
 'use strict';
 
 angular.module('firebaseHackathonApp')
-  .controller('listCtrl', ['$scope', '$http', 'angularFire',
-    function ($scope, $http, angularFire) {
-      var API_BASE = 'https://api.github.com/';
-    var profile = 'angular';
-    var project = 'angular.js';
-    var issues_url = API_BASE + 'repos/' + profile + '/' + project + '/issues';
-      $scope.issues = [];
-      $scope.get_issues = function() {
+.controller('listCtrl', ['$scope', '$http', 'angularFire', '$routeParams',
+	function ($scope, $http, angularFire, $routeParams) {
+		var API_BASE = 'https://api.github.com/';
+		var issues_url = API_BASE + 'repos/' + $routeParams.profile + '/' + $routeParams.project + '/issues';
+		$scope.issues = [];
+		$scope.get_issues = function() {
 
-        $http.get(issues_url).success(function(data) {
-          $scope.issues = data;
-        });
-      };
-      $scope.get_issues();
-      var key = profile+'@@'+project;
-      key = key.replace(/\./g, ',');
-      $scope.save = function(issue) {
-      	if (!$scope.repos[key])
-      		$scope.repos[key] = {};
-      	if (!$scope.repos[key][issue.id])
-      		$scope.repos[key][issue.id] = {};
-      	$scope.repos[key][issue.id][$scope.user.login] = issue.public_tags;
-      };
-    }]);
+			$http.get(issues_url).success(function(data) {
+				$scope.issues = data;
+			});
+		};
+
+		$scope.get_issues();
+		$scope.key = encode($routeParams.profile+'/'+$routeParams.project);
+
+		$scope.$watch('repos', function(newVal){
+			if (newVal && !newVal[$scope.key])
+				$scope.repos[$scope.key] = { issues: {}, tags: {}, options: {} };
+		});
+
+		$scope.save = function(issue) {
+			if (!$scope.repos[$scope.key][issue.id])
+				$scope.repos[$scope.key][issue.id] = { labels: {}, totals: {} };
+			var firebase = $scope.repos[$scope.key][issue.id];
+			firebase.labels[encode($scope.user.login)] = issue.public_tags;
+		};
+	}]);
+
+
+
+
+function encode(val) {
+	return encodeURIComponent(val).replace(/\./g, '%2E');
+}
+
+function decode(val) {
+	return decodeURIComponent(val);
+}
