a210ed77a83821d2b918c46eb7ff9f7d5627bb4d
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2013-08-13 20:35:01 -0700

    add controller for pulling out github issues

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('firebaseHackathonApp')
-  .controller('MainCtrl', ['$scope', 'angularFire', 'angularFireAuth', 
-    function ($scope, angularFire, angularFireAuth) {
+  .controller('list', ['$scope', '$http', 'angularFire', 'angularFireAuth',
+    function ($scope, $http, angularFire, angularFireAuth) {
       var API_BASE = 'https://api.github.com/';
 
       $scope.issues = [];
       $scope.get_issues = function() {
         var profile = 'angular';
         var project = 'angular.js';
-        var issues_url = API_BASE + profile + '/' + project + '/issues';
+        var issues_url = API_BASE + 'repos/' + profile + '/' + project + '/issues';
 
         $http.get(issues_url).success(function(data) {
           $scope.issues = data;
